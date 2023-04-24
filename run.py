bc2fcad0c71a002200288ac1ab9f7010883fb8bf
NikkyXO <alexisdame2017@gmail.com>
2023-04-24 15:59:20 +0100

    deployment

--- run.py
+++ run.py
@@ -2,11 +2,21 @@ from main import create_app
 from famapi.settings.extensions import mail
 from famapi.settings.database import Base, engine
 import os
+from flask import redirect
+
 
 app = create_app()
 mail.init_app(app)
 
 
+@app.route('/')
+def home():
+    return redirect('/ui')
+
+
+# app.add_url_rule('/', 'home', home)
+
 Base.metadata.create_all(bind=engine)
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=os.getenv("PORT", default=8000), debug=True)
+
