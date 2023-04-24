78fc732eed5ec21123833052d8de157b48c6839b
NikkyXO <alexisdame2017@gmail.com>
2023-04-24 16:14:01 +0100

    deployment

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from main import create_app
 from famapi.settings.extensions import mail
 from famapi.settings.database import Base, engine
+import os
 
 app = create_app()
 mail.init_app(app)
@@ -8,5 +9,4 @@ mail.init_app(app)
 
 Base.metadata.create_all(bind=engine)
 if __name__ == '__main__':
-
-    app.run(host="0.0.0.0", port=6998, debug=True)
+    app.run(host="0.0.0.0", port=os.getenv("PORT", default=8000), debug=True)
