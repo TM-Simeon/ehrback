992f084511f45f357e788bdfe7a0ca45678ce6ed
NikkyXO <alexisdame2017@gmail.com>
2023-04-24 15:08:40 +0100

    modify requirements

--- famapi/settings/database.py
+++ famapi/settings/database.py
@@ -5,7 +5,8 @@ import pymysql
 import os
 
 # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:feFsfZ3bV51O7F9F1ASZ@containers-us-west-89.railway.app:5665/railway"
+# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:feFsfZ3bV51O7F9F1ASZ@containers-us-west-89.railway.app:5665/railway"
+SQLALCHEMY_DATABASE_URI = "mysql://root:jMAoJVX7Ie6NOYF1GEct@containers-us-west-102.railway.app:6998/railway"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
