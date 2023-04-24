316f95cf930c4fdaf017e1521f40704a12c5d4a5
NikkyXO <alexisdame2017@gmail.com>
2023-04-24 16:10:16 +0100

    deployment

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from famapi.settings.extensions import mail
 from famapi.settings.database import Base, engine
 import os
 from flask import redirect
+import os
 
 
 app = create_app()
@@ -19,4 +20,3 @@ def home():
 Base.metadata.create_all(bind=engine)
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=os.getenv("PORT", default=8000), debug=True)
-
